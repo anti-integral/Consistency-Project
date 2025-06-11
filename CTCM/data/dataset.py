d6bfb41ae9ebec0d11d9b8a96d123b214eee3630
osanan25 <om.sanan007@gmail.com>
2025-06-11 03:04:10 -0400

    Fixed more imports

--- CTCM/data/dataset.py
+++ CTCM/data/dataset.py
@@ -1,9 +1,11 @@
+import torch
 import torchvision
 import torchvision.transforms as T
+from torch.utils.data import DataLoader
 
 def get_cifar_dataloader(batch_size: int, img_size: int, num_workers: int = 4):
     """
-    Returns a CIFAR‑10 training dataloader with simple augmentations.
+    Returns a CIFAR-10 training dataloader with simple augmentations.
     """
     transform = T.Compose([
         T.Resize(img_size),
@@ -15,6 +17,10 @@ def get_cifar_dataloader(batch_size: int, img_size: int, num_workers: int = 4):
     train_set = torchvision.datasets.CIFAR10(
         root="./data", train=True, download=True, transform=transform
     )
-    return torch.utils.data.DataLoader(
-        train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True
+    return DataLoader(
+        train_set,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=True
     )
